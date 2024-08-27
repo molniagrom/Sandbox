41b7cf326800769a4b0e3314eb688d88f3007f44
molnia_grom <mgrom250@gmail.com>
2024-08-27 23:12:21 +0300

    fifth

--- script.js
+++ script.js
@@ -27,3 +27,12 @@ console.log(rows[0]);
 
 // .......................................................
 
+let character = 'Hello';
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+let cities = ["London", "New York", "Mumbai"]
+console.log(cities);
+cities[2] = "Mexico City";
+console.log(cities);
+
+//.......................................
