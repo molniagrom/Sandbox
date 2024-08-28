98d535fdd809ea2f2e412a9c372d5399310b32eb
molnia_grom <mgrom250@gmail.com>
2024-08-28 11:05:52 +0300

    sixth

--- script.js
+++ script.js
@@ -36,3 +36,13 @@ cities[2] = "Mexico City";
 console.log(cities);
 
 //.......................................
+
+let character = 'Hello';
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+rows.push("freeCodeCamp");
+let popped = rows.pop();
+console.log(popped);
+console.log(rows);
+
+//......................................
