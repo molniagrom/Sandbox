344e9e580cc215d77765d7be8141a9e8717eb103
molnia_grom <mgrom250@gmail.com>
2024-08-28 18:12:21 +0300

    Step 69

--- FreeCodeCamp/script.js
+++ FreeCodeCamp/script.js
@@ -64,3 +64,25 @@ for (const row of rows) {
 }
 
 console.log(result);
+
+//.................................................
+// Step 69
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+    return " ".repeat(rowCount - rowNumber) + character.repeat(rowNumber) + " ".repeat(rowCount - rowNumber);
+}
+
+for (let i = 0; i < count; i = i + 1) {
+    rows.push(padRow(i + 1, count));
+}
+
+let result = ""
+
+for (const row of rows) {
+    result = result + "\n" + row;
+}
+
+console.log(result);
