a223d4cc5f9e6c0ebe6a41e4d860b211432eee1b
molnia_grom <mgrom250@gmail.com>
2024-08-28 11:27:33 +0300

    seventh

---
+++ CodeWars/tasks.js
@@ -0,0 +1,12 @@
+//Реализуйте функцию, которая преобразует заданное логическое значение в его строковое представление.
+
+function booleanToString(bool){
+
+    return bool.toString();
+
+    console.log(booleanToString(true));
+    console.log(booleanToString(false));
+}
+
+//........................................................................
+
