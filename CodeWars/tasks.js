33c5cd2c80f2a9efe834007e4f84e81d1a82d88b
molnia_grom <mgrom250@gmail.com>
2024-08-29 20:38:16 +0300

    Updated tasks.js

--- CodeWars/tasks.js
+++ CodeWars/tasks.js
@@ -36,5 +36,15 @@ function greet() {
 }
 greet()
 
+// .............................................
 
+// Convert boolean values to strings 'Yes' or 'No'.
+
+function boolToWord( bool ){
+    if(bool === true) {
+        return "Yes";
+    } else if (bool === false) {
+        return "No"
+    }
+}
 
