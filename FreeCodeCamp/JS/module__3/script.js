b720e3257b3afd373e229751fa9554ed308a7b6a
molnia_grom <mgrom250@gmail.com>
2024-09-02 13:10:23 +0300

    Step 103

--- FreeCodeCamp/JS/module__3/script.js
+++ FreeCodeCamp/JS/module__3/script.js
@@ -86,14 +86,28 @@ function buyHealth() {
 }
 
 function buyWeapon() {
-    if (gold >= 30) {
-        gold -= 30;
-        currentWeaponIndex++;
-        goldText.innerText = gold;
-        let newWeapon = weapons[currentWeaponIndex].name;
-        text.innerText = "You now have a " + newWeapon + ".";
-        inventory.push(newWeapon);
-        text.innerText += " In your inventory you have: ";
+    if (currentWeaponIndex < weapons.length - 1) {
+        if (gold >= 30) {
+            gold -= 30;
+            currentWeaponIndex++;
+            goldText.innerText = gold;
+            let newWeapon = weapons[currentWeaponIndex].name;
+            text.innerText = "You now have a " + newWeapon + ".";
+            inventory.push(newWeapon);
+            text.innerText += " In your inventory you have: " + inventory;
+        } else {
+            text.innerText = "You do not have enough gold to buy a weapon.";
+        }
+    } else {
+        text.innerText = "You already have the most powerful weapon!";
+        button2.innerText = "Sell weapon for 15 gold";
+        button2.onclick = sellWeapon;
+    }
+}
+
+function sellWeapon() {
+    if (inventory.length > 1) {
+
     }
 }
 
