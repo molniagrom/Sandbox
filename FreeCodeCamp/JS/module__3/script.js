a814fda97b087c463e7a9d6fd7edf00457ad7e52
molnia_grom <mgrom250@gmail.com>
2024-09-02 11:59:39 +0300

    Step 82

--- FreeCodeCamp/JS/module__3/script.js
+++ FreeCodeCamp/JS/module__3/script.js
@@ -16,6 +16,12 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+const weapons = [
+    { name: 'stick', power: 5 },
+    { name: 'dagger', power: 30 },
+    { name: 'claw hammer', power: 50 },
+    { name: 'sword', power: 100 }
+];
 const locations = [
     {
         name: "town square",
@@ -29,7 +35,12 @@ const locations = [
         "button functions": [buyHealth, buyWeapon, goTown],
         text: "You enter the store."
     },
-
+    {
+        name: "cave",
+        "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
+        "button functions": [fightSlime, fightBeast, goTown],
+        text: "You enter the cave. You see some monsters."
+    }
 ];
 
 // initialize buttons
@@ -56,7 +67,7 @@ function goStore() {
 }
 
 function goCave() {
-    console.log("Going to cave.");
+    update(locations[2]);
 }
 
 function fightDragon() {
@@ -64,11 +75,20 @@ function fightDragon() {
 }
 
 function buyHealth() {
-
+    if (gold >= 10) {
+        gold -= 10;
+        health += 10;
+        goldText.innerText = gold;
+        healthText.innerText = health;
+    } else {
+        text.innerText = "You do not have enough gold to buy health.";
+    }
 }
 
 function buyWeapon() {
+    if (gold >= 30) {
 
+    }
 }
 
 function fightSlime() {
