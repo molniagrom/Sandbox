2554d5123232d553eef16f52fb276452f0a69d3c
molnia_grom <mgrom250@gmail.com>
2024-09-05 17:05:05 +0300

    Step 5

--- FreeCodeCamp/JS/module__4/script.js
+++ FreeCodeCamp/JS/module__4/script.js
@@ -10,12 +10,19 @@ const darkColorsArr = [
     "#2C3E50",
     "#800020",
 ];
+
 function getRandomIndex() {
     const randomIndex = Math.floor(darkColorsArr.length * Math.random());
     return randomIndex;
 }
 
 const body = document.querySelector("body");
-const bgHexCodeSpanElement = document.querySelector("bg-hex-code");
+const bgHexCodeSpanElement = document.querySelector("#bg-hex-code");
 
-console.log(bgHexCodeSpanElement);
+function changeBackgroundColor() {
+    const color = darkColorsArr[getRandomIndex];
+
+    bgHexCodeSpanElement.innerText = color;
+    body.style.backgroundColor = color;
+}
+changeBackgroundColor();
