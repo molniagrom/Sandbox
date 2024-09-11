f60f644520731de1ca876113831b665cb456e254
molnia_grom <mgrom250@gmail.com>
2024-09-11 18:14:35 +0300

    changes

--- CodeWars/tasks__2.js
+++ CodeWars/tasks__2.js
@@ -406,21 +406,22 @@
 //+9собачьи годы для второго года
 //+5собачьи годы за каждый последующий год.
 
-var humanYearsCatYearsDogYears = function(humanYears) {
-      let catYears = 0;
-        let dogYears = 0;
-          
-            if (humanYears === 1) {
-                catYears = 15;
-                    dogYears = 15;
-                      } else if (humanYears === 2) {
-                          catYears = 15 + 9;
-                              dogYears = 15 + 9;
-                                } else if (humanYears > 2) {
-                                    catYears = 15 + 9 + (humanYears - 2) * 4;
-                                        dogYears = 15 + 9 + (humanYears - 2) * 5;
-                                          }
-                                            
-                                              return [humanYears, catYears, dogYears];
-                                              }
-  //………..................                                            
+// var humanYearsCatYearsDogYears = function (humanYears) {
+//     let catYears = 0;
+//     let dogYears = 0;
+//
+//     if (humanYears === 1) {
+//         catYears = 15;
+//         dogYears = 15;
+//     } else if (humanYears === 2) {
+//         catYears = 15 + 9;
+//         dogYears = 15 + 9;
+//     } else if (humanYears > 2) {
+//         catYears = 15 + 9 + (humanYears - 2) * 4;
+//         dogYears = 15 + 9 + (humanYears - 2) * 5;
+//     }
+//
+//     return [humanYears, catYears, dogYears];
+// }
+// .......................................................................................
+
