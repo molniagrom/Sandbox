dadd288bef3d7216a69e842d287fc977ce03d2e3
molnia_grom <mgrom250@gmail.com>
2024-09-11 18:32:08 +0300

    new tasks changes

--- CodeWars/tasks__3.js
+++ CodeWars/tasks__3.js
@@ -46,6 +46,8 @@
 //         return "Underweight";
 //     } else if(bmiValue <= 25.0) {
 //         return "Normal"
+//     } else if(bmiValue <= 30.0) {
+//         return "Overweight"
 //     } else if(bmiValue > 30) {
 //         return "Obese"
 //     }
