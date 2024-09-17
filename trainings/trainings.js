e176562daa7026aa138a54b7cf4ce43cba0997a0
molnia_grom <mgrom250@gmail.com>
2024-09-17 12:52:20 +0300

    new trainings

--- trainings/trainings.js
+++ trainings/trainings.js
@@ -215,19 +215,24 @@
 
 // Умножаем на 2
 
-let menu = {
-    width: 200,
-    height: 300,
-    title: "My menu"
-};
+// let menu = {
+//     width: 200,
+//     height: 300,
+//     title: "My menu"
+// };
+//
+// function multiplyNumeric(menu) {
+//     for (let key in menu) {
+//         if (typeof menu[key] === 'number') {
+//             menu[key] *= 2; // Умножаем на 2 и сохраняем результат
+//         }
+//     }
+// }
 
-function multiplyNumeric(menu) {
-    for (let key in menu) {
-        if (typeof menu[key] === 'number') {
-            menu[key] *= 2; // Умножаем на 2 и сохраняем результат
-        }
-    }
-}
+// ..........................................................................
+
+
+
 
 
 
