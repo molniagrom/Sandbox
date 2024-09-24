f6df6cc129876053f90e4c5230a9df1636407b5f
molnia_grom <mgrom250@gmail.com>
2024-09-24 10:52:45 +0300

    new tasks

--- CodeWars/tasks__4.js
+++ CodeWars/tasks__4.js
@@ -385,3 +385,41 @@ function validatePIN(pin) {
 
 // ..................................................................................................................................
 
+// Задача
+// Я написал пять функций. Каждая функция получает параметр arr, который является массивом. Завершите функции, используя arrвнутри тел функций.
+
+// 1. getLength(arr)    should return length of arr
+// 2. getFirst(arr)     should return the first element of arr
+// 3. getLast(arr)      should return the last element of arr
+// 4. pushElement(arr)  should push an element to arr, and then return arr
+// 5. popElement(arr)   should pop an element from arr, and then return arr
+// Закончив работу, нажмите «Выполнить тесты», чтобы проверить, правильно ли работает ваш код.
+
+// В конце нажмите «Отправить», чтобы отправить свой код для прохождения этого ката.
+
+// function getLength(arr){
+//   //return length of arr
+//   return arr.length;
+// }
+// function getFirst(arr){
+//   //return the first element of arr
+//   return arr[0];
+// }
+// function getLast(arr){
+//   //return the last element of arr
+//   return arr[arr.length-1];
+// }
+// function pushElement(arr){
+//   var el=4;
+//   //push el to arr
+//   arr.push(el);
+//   return arr;
+// }
+// function popElement(arr){
+//   //pop an element from arr
+//   arr.pop();
+//   return arr;
+// }
+
+// ......................................................................................
+
