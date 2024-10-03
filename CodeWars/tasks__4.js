459d33a6961e54a3827e7cabe05becaf18f077b3
molnia_grom <mgrom250@gmail.com>
2024-10-03 12:01:49 +0300

    new tasks

--- CodeWars/tasks__4.js
+++ CodeWars/tasks__4.js
@@ -1150,3 +1150,23 @@ function remove (string) {
 
 // ......................................................................................
 
+// Создайте функцию, которая отвечает на вопрос «Вы играете на банджо?».
+// Если ваше имя начинается с буквы «R» или строчной «r», вы играете на банджо!
+
+// Функция принимает имя в качестве единственного аргумента и возвращает одну из следующих строк:
+
+// name + " plays banjo" 
+// name + " does not play banjo"
+// Указанные имена всегда являются допустимыми строками.
+
+// Я САМА!!!!!!!!!!!!!!!!!!!!!!!!!
+function areYouPlayingBanjo(name) {
+  let r = "R";
+
+return name.toLowerCase().startsWith(r.toLowerCase()) ? name + " plays banjo" : name + " does not play banjo";
+  // if (name.toLowerCase().startsWith(r.toLowerCase())) {
+  //   return name + " plays banjo";
+  // } else{
+  //   return name + " does not play banjo";
+  // }
+}
