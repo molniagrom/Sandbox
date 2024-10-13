003bfb6b27b6ca64bbfa783aa68b722a88b870f9
molnia_grom <mgrom250@gmail.com>
2024-10-13 15:15:26 +0300

    step 14

--- FreeCodeCamp/JS/module__7/script.js
+++ FreeCodeCamp/JS/module__7/script.js
@@ -5,4 +5,43 @@ const nextButton = document.getElementById("next");
 const previousButton = document.getElementById("previous");
 const shuffleButton = document.getElementById("shuffle");
 
-let allSongs = [];
+let allSongs = [
+  {
+    id: 0,
+    title: "Scratching The Surface",
+    artist: "Quincy Larson",
+    duration: "4:25",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/scratching-the-surface.mp3",
+  },
+  {
+    id: 1,
+    title: "Can't Stay Down",
+    artist: "Quincy Larson",
+    duration: "4:15",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/can't-stay-down.mp3",
+  },
+  {
+    id: 1,
+    title: "Can't Stay Down",
+    artist: "Quincy Larson",
+    duration: "4:15",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/can't-stay-down.mp3",
+  },
+];
+
+const audio = new Audio();
+let userData = {
+  songs: [...allSongs],
+  currentSong: null,
+  songCurrentTime: 0,
+};
+
+const printGreeting = () => {
+  console.log("Hello there!");
+}
+
+const printMessage = (org) => {
+  console.log(`${org} is awesome!`);
+}
+
+printMessage("freeCodeCamp");
