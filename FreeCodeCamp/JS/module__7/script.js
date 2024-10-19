081f0529e41bd52a2cbfe0e31fb95aaa032f8b9a
molnia_grom <mgrom250@gmail.com>
2024-10-19 14:03:16 +0300

    Step 49

--- FreeCodeCamp/JS/module__7/script.js
+++ FreeCodeCamp/JS/module__7/script.js
@@ -95,7 +95,17 @@ const playSong = (id) => {
   } else {
     audio.currentTime = userData?.songCurrentTime;
   }
+  userData.currentSong = song;
+  playButton.classList.add("playing");
 
+  audio.play();
+};
+
+const pauseSong = () => {
+  userData.songCurrentTime = audio.currentTime;
+  
+  playButton.classList.remove("playing");
+  audio.pause();
 };
 
 const renderSongs = (array) => {
@@ -103,7 +113,7 @@ const renderSongs = (array) => {
     .map((song)=> {
       return `
       <li id="song-${song.id}" class="playlist-song">
-      <button class="playlist-song-info">
+      <button class="playlist-song-info" onclick="playSong(${song.id})">
           <span class="playlist-song-title">${song.title}</span>
           <span class="playlist-song-artist">${song.artist}</span>
           <span class="playlist-song-duration">${song.duration}</span>
@@ -119,6 +129,16 @@ const renderSongs = (array) => {
 
   playlistSongs.innerHTML = songsHTML;
 };
+
+playButton.addEventListener("click", () => {
+    if (userData?.currentSong === null) {
+    playSong(userData?.songs[0].id);
+  } else {
+    playSong(userData?.currentSong.id);
+  }
+});
+
+
 
 const sortSongs = () => {
   userData?.songs.sort((a,b) => {
