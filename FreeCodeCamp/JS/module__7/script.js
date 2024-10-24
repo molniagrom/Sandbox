88bb8b1cd6545bbb1980f267ae0f51b2a54c2e27
molnia_grom <mgrom250@gmail.com>
2024-10-24 17:55:43 +0300

    playlist is ready

--- FreeCodeCamp/JS/module__7/script.js
+++ FreeCodeCamp/JS/module__7/script.js
@@ -163,7 +163,17 @@ const deleteSong = (id) => {
 
     resetButton.id = "reset";
     resetButton.ariaLabel = "Reset playlist";
-  
+    resetButton.appendChild(resetText);
+    playlistSongs.appendChild(resetButton);
+
+    resetButton.addEventListener("click", () => {
+      userData.songs = [...allSongs];
+
+      renderSongs(sortSongs()); 
+      setPlayButtonAccessibleText();
+      resetButton.remove();
+    });
+
   }
 
 };
@@ -240,6 +250,22 @@ previousButton.addEventListener("click", playPreviousSong);
 
 shuffleButton.addEventListener("click", shuffle);
 
+audio.addEventListener("ended", () => {
+  const currentSongIndex = getCurrentSongIndex();
+  const nextSongExists = userData?.songs[currentSongIndex + 1] !== undefined;
+
+    if (nextSongExists) {
+      playNextSong();
+    } else {
+      userData.currentSong = null;
+      userData.songCurrentTime = 0;  
+pauseSong()
+setPlayerDisplay()
+highlightCurrentSong()
+setPlayButtonAccessibleText()
+    }
+});
+
 const sortSongs = () => {
   userData?.songs.sort((a,b) => {
     if (a.title < b.title) {
@@ -257,3 +283,4 @@ const sortSongs = () => {
 };
 
 renderSongs(sortSongs());
+setPlayButtonAccessibleText();
