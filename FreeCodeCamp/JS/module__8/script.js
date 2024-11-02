64436d1dcddc7bfe253079e5bbf0e5ffc8f3ab2f
molnia_grom <mgrom250@gmail.com>
2024-11-02 13:25:48 +0200

    end

--- FreeCodeCamp/JS/module__8/script.js
+++ FreeCodeCamp/JS/module__8/script.js
@@ -15,7 +15,15 @@ dateOptionsSelectElement.addEventListener("change", () => {
 
   switch (dateOptionsSelectElement.value) {
     case "yyyy-mm-dd":
-      currentDateParagraph.textContent = formattedDate.split("-").reverse().join("-").
-
+      currentDateParagraph.textContent = formattedDate
+        .split("-")
+        .reverse()
+        .join("-");
+      break;
+    case "mm-dd-yyyy-h-mm":
+      currentDateParagraph.textContent = `${month}-${day}-${year} ${hours} Hours ${minutes} Minutes`;
+      break;
+    default: 
+    currentDateParagraph.textContent = formattedDate
   }
 });
