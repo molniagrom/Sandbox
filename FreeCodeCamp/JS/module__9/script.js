bb2fc9ee57631dcc0493c8933e58a2bb8e83a439
molnia_grom <mgrom250@gmail.com>
2025-01-02 18:12:41 +0200

    Step 40

--- FreeCodeCamp/JS/module__9/script.js
+++ FreeCodeCamp/JS/module__9/script.js
@@ -200,7 +200,11 @@ playersDropdownList.addEventListener("change", (e) => {
   playerCards.innerHTML = "";
 
 
+  switch (e.target.value) {
+    case "nickname":
+      setPlayerCards(players.filter((player) => player.nickname !== null));
 
+  }
 
 
 });
