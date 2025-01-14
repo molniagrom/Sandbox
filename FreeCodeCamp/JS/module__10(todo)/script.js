c26e74b8176db5193598b5f17f88a8d9bb3fc291
molnia_grom <mgrom250@gmail.com>
2025-01-14 15:42:23 +0200

    Step 1

---
+++ FreeCodeCamp/JS/module__10(todo)/script.js
@@ -0,0 +1,31 @@
+const taskForm = document.getElementById('taskForm');
+const confirmCloseDialog = document.getElementById('confirm-close-dialog');
+const openTaskFormBtn = document.getElementById('open-task-form-btn');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
