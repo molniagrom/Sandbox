8c6199432a8bdc7e97f50d68af474d32c6ac5246
molnia_grom <mgrom250@gmail.com>
2025-01-30 19:48:31 +0200

    new tasks

--- CodeWars/tasks__4.js
+++ CodeWars/tasks__4.js
@@ -2625,8 +2625,44 @@ function arithmetic(a, b, operator){
  }
 }
 
+function correctPolishLetters (string) {
+
 //.............................................................................
 
+//     There are 32 letters in the Polish alphabet: 9 vowels and 23 consonants.
+//
+//         Your task is to change the letters with diacritics:
+//
+//     ą -> a,
+//         ć -> c,
+//         ę -> e,
+//         ł -> l,
+//         ń -> n,
+//         ó -> o,
+//         ś -> s,
+//         ź -> z,
+//         ż -> z
+//     and print out the string without the use of the Polish letters.
+//
+//         For example:
+//
+//         "Jędrzej Błądziński"  -->  "Jedrzej Bladzinski"
+
+    const polishToLatin = {
+        "ą": "a",
+        "ć": "c",
+        "ę": "e",
+        "ł": "l",
+        "ń": "n",
+        "ó": "o",
+        "ś": "s",
+        "ź": "z",
+        "ż": "z"
+    };
+return string.split("").map(w => polishToLatin[w] ?  polishToLatin[w] : w).join('')
+}
+
+//.............................................................................
 
 
 
