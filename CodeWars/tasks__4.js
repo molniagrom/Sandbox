c0a82504f515a65ba603c99e2f087c11fabaf176
molnia_grom <mgrom250@gmail.com>
2025-02-01 16:21:47 +0200

    task new

--- CodeWars/tasks__4.js
+++ CodeWars/tasks__4.js
@@ -2664,6 +2664,13 @@ return string.split("").map(w => polishToLatin[w] ?  polishToLatin[w] : w).join(
 
 //.............................................................................
 
+// Oh no! Timmy hasn't followed instructions very carefully and forgot how to use the new String Template feature, Help Timmy with his string template so it works as he expects!
+
+function buildString(...template){
+    return `I like ${template.join(', ')}!`;
+}
+
+//.............................................................................
 
 
 
