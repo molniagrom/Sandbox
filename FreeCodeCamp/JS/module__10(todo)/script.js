3d9700ae538cd8ca3a1e5616a918efa1de979306
molnia_grom <mgrom250@gmail.com>
2025-02-14 16:13:49 +0200

    step 24

--- FreeCodeCamp/JS/module__10(todo)/script.js
+++ FreeCodeCamp/JS/module__10(todo)/script.js
@@ -46,7 +46,9 @@ taskForm.addEventListener("submit", (e) => {
 
     taskData.forEach(({id, title, date, description}) => {
             tasksContainer.innerHTML += `
-          
+        <div class="task" id="${id}">
+        
+        </div>
       `
         }
     );
