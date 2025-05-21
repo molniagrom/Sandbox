113712f91c9a056d3431136c2fb1a37f6d9ee65e
molnia_grom <mgrom250@gmail.com>
2025-05-21 11:38:25 +0300

    new task

--- CodeWars/tasks__4.js
+++ CodeWars/tasks__4.js
@@ -2940,8 +2940,18 @@ console.log(lowercaseCount("gdfgdgf"))
 
 //........................................................................
 
+// This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+// Add the value "codewars" to the array websites 1,000 times.
 
+let websites = []
 
+  for (let i = 1; i <= 1000; i++) {
+      websites.push("codewars")
+  }
+
+console.log(websites)
+
+//........................................................................
 
 
 
