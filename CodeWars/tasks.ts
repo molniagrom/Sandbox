30aa7f5b6eb2c43d28deb6269cc0469090ce3de8
molnia_grom <mgrom250@gmail.com>
2025-08-28 13:52:25 +0300

    new task

---
+++ CodeWars/tasks.ts
@@ -0,0 +1,24 @@
+// const updateArray = (array: string[], value: string) => {
+//     return array.includes(value)
+// }
+//
+// const updateArray2 = (array: number[], value: number) => {
+//     return array.includes(value)
+// }
+
+// function generic
+
+// const updateArray = <T>(array: T[], value: T) => {
+//     return array.includes(value)
+// }
+//
+// // Строки
+// const stringArray = ["apple", "banana", "cherry"]
+// console.log(updateArray(stringArray, "banana")) // ['apple', 'banana', 'cherry']
+// console.log(updateArray(stringArray, "strawberry")) // ['apple', 'banana', 'cherry', 'strawberry']
+//
+// // Числа
+// const numberArray = [1, 2, 3]
+// console.log(updateArray(numberArray, 2)) // [1, 2, 3]
+// console.log(updateArray(numberArray, 4)) // [1, 2, 3, 4]
+
